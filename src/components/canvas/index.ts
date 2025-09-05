c3bbe1bc86966cd827606a218102dc44c7862016
Richard Bausek <richardb@uniform.dev>
2025-09-05 12:14:29 +0200

    Initial setup

---
+++ src/components/canvas/index.ts
@@ -0,0 +1,7 @@
+import baseCskComponentsMapping from '@uniformdev/csk-components/components/canvas';
+import Button from './Button';
+
+export const cskComponentsMapping = {
+  ...baseCskComponentsMapping,
+  button: { component: Button },
+};
